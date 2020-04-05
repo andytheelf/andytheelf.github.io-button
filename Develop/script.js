0c1df55261b8e2d4f65bc96b5a507a70108b5100
andytheelf <61925665+andytheelf@users.noreply.github.com>
2020-04-05 16:09:25 -0500

    Add files via upload

---
+++ Develop/script.js
@@ -0,0 +1,69 @@
+//Password Variables
+var inputCharacters = "";
+var prompts = "";
+
+//Variable Character Sets
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var specialSet = "@#$%^&*+<>?[]{}=~/";
+  var numericValue = "0123456789";
+
+ //Generate Password Function
+function generatePassword() {
+ var entry = Number(window.prompt("Please choose a password length between 8-128 characters!"));
+ 
+ //Password if statements
+     if (entry > 8 || entry > 128) {
+       window.confirm("Invalid"); 
+  }
+     if (entry > 8 && entry < 128) {
+       console.log(entry);
+     
+       (upper = window.confirm("Will this contain uppercase letters?"))
+     if (upper == true) {
+       inputCharacters += upperCase;
+     }
+     
+     (lower = window.confirm("Will this contain lowercase letters?"))
+        if (lower == true){
+          inputCharacters += lowerCase;
+        }
+        (special = window.confirm("Will this contain special characters?"))
+        if (special == true){
+          inputCharacters += specialSet;
+        }
+       
+        (numeric = window.confirm("Will this contain numbers?"))
+        if (numeric == true){
+          inputCharacters += numericValue;
+        }
+        
+   
+   //Create for loop for Random generation
+   var prompts = []; 
+   
+   for (i = 0; i < entry; i++) {
+     prompts.push(inputCharacters[Math.floor((Math.random() * inputCharacters.length) + 1)]);
+     var product = prompts.join("");
+   }
+      alert(product);
+   }
+  
+  };
+
+
+
+// Get references to the #generate element
+ var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# andytheelf.github.io-button
+# Password Generator 
+
+## JavaScript Introduction
+
+### Click the button to initiate generate your password! 
+
+![GitHub Logo](./images/button.png)
+Format: ![Alt Text](url)
+
+#### Enter your password length!
+![GitHub Logo](./images/length.png)
+Format: ![Alt Text](url)
+
+#### CLick 'ok' to add a character set, or cancel to exclude it! 
+![GitHub Logo](./images/verify.png)
+Format: ![Alt Text](url)
+
+
+#### Your random password has generated! 
+
+![GitHub Logo](./images/complete.png)
+Format: ![Alt Text](url)
+
